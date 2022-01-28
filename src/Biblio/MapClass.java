baceddda57bce3a7c6c81b6d074ce079b0e6998f
Ott Pikk <ott@karmai.eu>
2022-01-28 17:35:09 +0200

    TemperatureConversion

--- src/Biblio/MapClass.java
+++ src/Biblio/MapClass.java
@@ -1,13 +1,45 @@
 package Biblio;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapClass {
     public static void main(String[] args) {
 
+        String[] allNames = {
+                "Jane", "Adams",
+                "Muhammad", "Ali",
+                "Stephan", "Ambrose",
+                "Louis", "Armstrong",
+                "Joan", "Baez",
+                "Josephine", "Baker",
+                "Eleanor", "Roosevelt",
+                "Frank", "Sinatra"
+        };
 
-        Map<String, String> famousPeople = new HashMap<>();
+        // Put first names and last names into separate Arrays
+        String[] firstNames = new String[allNames.length/2];
+        String[] lastNames = new String[allNames.length/2];
+        for (int i = 0; i < allNames.length; i++){
+            if (i %2 == 0){
+                firstNames[i/2] = allNames[i];
+            } else {
+                lastNames[i/2] = allNames[i];
+            }
+        }
+        // Check if works
+        System.out.println(Arrays.toString(firstNames));
+        System.out.println(Arrays.toString(lastNames));
+
+        //Put the information into map
+        Map<String, String> famousPeople = new HashMap<String, String>();
+        for (int i = 0; i < lastNames.length; i++){
+            famousPeople.put(lastNames[i], firstNames[i]);
+        }
+        //Call the names
+        System.out.println(famousPeople.get("Adams"));
+        System.out.println(famousPeople.get("Sinatra"));
 
     }
 }
