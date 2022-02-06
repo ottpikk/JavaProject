b75c647b53d13d88d0fc024686552ae9b6928aa1
Ott Pikk <ott@karmai.eu>
2022-02-06 13:42:39 +0200

    Enum and Interface

---
+++ src/advanced1/EnumExample.java
@@ -0,0 +1,34 @@
+package advanced1;
+
+enum Level{
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+public class EnumExample {
+    public static void main(String[] args) {
+
+        Level myLevel;
+
+        Double numOfRaces = Math.random() * 10;
+        if (numOfRaces >= 10){
+            myLevel = Level.HIGH;
+        } else if (numOfRaces >= 5){
+            myLevel = Level.MEDIUM;
+        } else {
+            myLevel = Level.LOW;
+        }
+
+        switch (myLevel){
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+        }
+    }
+}
