d45e717d1871b24cdabe9cce7a6e05b00217f4b7
Ott Pikk <ott@karmai.eu>
2022-02-12 09:01:08 +0200

    maximumDistance

--- src/advanced1/MaximumDistance.java
+++ src/advanced1/MaximumDistance.java
@@ -1,14 +1,16 @@
 package advanced1;
 
+import java.util.Scanner;
+
 class Vehicle{
     protected float fuel;
     protected float fuelUsage;
     protected int passengers;
 
-    public void maxDistance(){
-        fuelUsage = fuelUsage + (fuelUsage * passengers * (float)0.05) ;
+    public double maxDistance(){
+        fuelUsage = fuelUsage + (fuelUsage * passengers * 0.05f) ;
         double maxDistance = (double)Math.round(100 * fuel / fuelUsage * 100)/100;
-        System.out.println("Possible maximum distance: " + maxDistance + " km");
+        return maxDistance;
     }
 
     public Vehicle(double fuel, double fuelUsage, int passengers){
@@ -26,32 +28,46 @@ class Car  extends Vehicle{
         this.airConditioner = airConditioner;
     }
 
-    @Override
-    public void maxDistance() {
+
+    public double maxDistance() {
 
         if (airConditioner){
-            fuelUsage = (float)1.1 * (fuelUsage + (fuelUsage * passengers * (float)0.05));
+            fuelUsage = 1.1f * (fuelUsage + (fuelUsage * passengers * 0.05f));
         } else {
-            fuelUsage = fuelUsage + (fuelUsage * passengers * (float)0.05) ;
+            fuelUsage = fuelUsage + (fuelUsage * passengers * 0.05f) ;
         }
         double maxDistance = (double)Math.round(100 * fuel / fuelUsage * 100)/100;
-        System.out.println("Possible maximum distance: " + maxDistance + " km");
+        return maxDistance;
     }
 }
 
 public class MaximumDistance {
     public static void main(String[] args) {
-        Vehicle vehicle1 = new Vehicle(55.5,5.55,0);
-        vehicle1.maxDistance();
+        Scanner scanner = new Scanner(System.in);
 
-        Vehicle vehicle2 = new Vehicle(55.5,5.5,5);
-        vehicle2.maxDistance();
+        System.out.println("Enter fuel amount in tank:");
+        float fuel = scanner.nextFloat();
+        System.out.println("Enter your car fuel usage (l/100km):");
+        float fuelUsage = scanner.nextFloat();
+        System.out.println("Enter number of passengers:");
+        int passengers = scanner.nextInt();
+        System.out.println("Is your A/C running? (y / n)");
+        boolean airConditioner = scanner.next().charAt(0) == 'y';
 
-        Car car1 = new Car(70.0,7.0,0,true);
-        car1.maxDistance();
+        Car car3 = new Car(fuel,fuelUsage,passengers,airConditioner);
+        Car car1 = new Car(fuel,fuelUsage,0,false);
+        Car car2 = new Car(fuel,fuelUsage,passengers,false);
+        Car car4 = new Car(fuel,fuelUsage,0,airConditioner);
 
-        Car car2 = new Car(70.0,7.0,0,false);
-        car2.maxDistance();
+        System.out.println("Your car can drive:");
+
+        System.out.println("1. With parameters you entered     - " +car3.maxDistance()+ " km");
+        System.out.println("2. Without passengers, with A/C    - " +car4.maxDistance()+ " km");
+        System.out.println("3. With passengers, without A/C    - " +car2.maxDistance()+ " km");
+        System.out.println("4. Without passengers, without A/C - " +car1.maxDistance()+ " km");
+
+
+
 
     }
 }
