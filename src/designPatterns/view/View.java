9f3b23df7e1e1aa94126d1429db406d9994e8e43
Ott Pikk <ott@karmai.eu>
2022-03-29 08:14:35 +0300

    add window

--- src/designPatterns/view/View.java
+++ src/designPatterns/view/View.java
@@ -11,5 +11,19 @@ public class View extends JFrame {
     public View(Model model) throws HeadlessException {
         super("MVC Demo");
         this.model = model;
+
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.anchor = GridBagConstraints.CENTER;
+        gridBagConstraints.gridheight = 0;
+        gridBagConstraints.gridwidth = 0;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+
+        setSize(600,500);   // Size of window
+        setDefaultCloseOperation(EXIT_ON_CLOSE);  //set method to end running
+        setVisible(true);  // set if window is visible to user
     }
 }
